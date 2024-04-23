71012ad5f5aba69a3a7dc4b7220983a1a49b6d53
JPabloLobato <jlobagon@myuax.com>
2024-04-23 16:35:01 +0200

    Commit ListaDeParejas

---
+++ Gestion_Datos_Dinamicos/src/main/java/groupId/ListaDeParejas.java
@@ -0,0 +1,26 @@
+package groupId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeParejas<T, U> {
+    private List<Pareja<T, U>> lista;
+
+    public ListaDeParejas() {
+        this.lista = new ArrayList<>();
+    }
+
+    public void agregarPareja(Pareja<T, U> pareja) {
+        lista.add(pareja);
+    }
+
+    public void eliminarPareja(int indice) {
+        if (indice >= 0 && indice < lista.size()) {
+            lista.remove(indice);
+        }
+    }
+
+    public List<Pareja<T, U>> getLista() {
+        return lista;
+    }
+}
