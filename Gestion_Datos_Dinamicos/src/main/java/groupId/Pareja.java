196ff5aa32468ee5521a39fb17b1b00a93a332fd
JPabloLobato <jlobagon@myuax.com>
2024-04-23 17:02:10 +0200

    Commit Pareja

--- Gestion_Datos_Dinamicos/src/main/java/groupId/Pareja.java
+++ Gestion_Datos_Dinamicos/src/main/java/groupId/Pareja.java
@@ -1,27 +1,11 @@
 package groupId;
 
 public class Pareja<T, U> {
-    private T primero;
-    private U segundo;
+    private final T primero;
+    private final U segundo;
 
     public Pareja(T primero, U segundo) {
         this.primero = primero;
-        this.segundo = segundo;
-    }
-
-    public T getPrimero() {
-        return primero;
-    }
-
-    public void setPrimero(T primero) {
-        this.primero = primero;
-    }
-
-    public U getSegundo() {
-        return segundo;
-    }
-
-    public void setSegundo(U segundo) {
         this.segundo = segundo;
     }
 
