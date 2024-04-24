31feef2a2b06ccb080ff8cffa11553cde6375950
JPabloLobato <jlobagon@myuax.com>
2024-04-24 09:52:34 +0200

    Commit ListaDeParejas

---
+++ Gestion_Datos_Dinamicos/src/test/java/groupId/ListaDeParejasTest.java
@@ -0,0 +1,12 @@
+package groupId;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListaDeParejasTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
